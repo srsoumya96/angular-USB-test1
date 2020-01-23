47f3e1e5f790809d0c0b909babb510121a047424
srsoumya96 <sr.soumya96@gmail.com>
2020-01-23 14:00:12 +0530

    app.module updated

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,6 +2,8 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { routing } from './app-routing';
+import { HttpClientModule } from '@angular/common/http';
+import { HttpClientInMemoryWebApiModule } from 'angular-in-memory-web-api';
 
 import { AppComponent } from './app.component';
 import { SignupComponent } from './signup/signup.component';
@@ -10,9 +12,10 @@ import { DashboardComponent } from './dashboard/dashboard.component';
 import { RestService } from './rest.service';
 import { SigninComponent } from './signin/signin.component';
 import { FooterComponent } from './footer/footer.component';
+import { modelLogin } from './modelLogin';
 
 @NgModule({
-  imports:      [ BrowserModule, FormsModule, routing, ReactiveFormsModule ],
+  imports:      [ BrowserModule, FormsModule, routing, ReactiveFormsModule, HttpClientInMemoryWebApiModule.forRoot(modelLogin), HttpClientModule ],
   declarations: [ AppComponent, SignupComponent, HeaderComponent, DashboardComponent, SigninComponent, FooterComponent ],
   bootstrap:    [ AppComponent ],
   providers: [RestService]
